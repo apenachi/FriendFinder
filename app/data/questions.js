70dffaba48e7a25fd209a48d11ee1f16b484216d
apenachi <apenachi@yahoo.com>
2016-08-06 09:58:11 -0400

    Friend Finder Heroku Ready

--- app/data/questions.js
+++ app/data/questions.js
@@ -3,12 +3,12 @@ var questions = [
 	{question:'You always greet when you walk into a room'},
 	{question:'How confident are you to start a conversation'},
 	{question:'Favorite place to hangout is a club'},
-	// {question:'You like to be comforted when you’re sad or upset'},
-	// {question:'You do not hold back, you say what you feel'},
-	// {question:'The more you spent in clothes the better you look'},
-	// {question:'The truth even if it hurts'},
-	// {question:'Like to go out socially with more than 5 people'},
-	// {question:'If you are in a rush, would you stop to help someone'},
-	// {question:'How bossy can you get'}
+	{question:'You like to be comforted when you’re sad or upset'},
+	{question:'You do not hold back, you say what you feel'},
+	{question:'The more you spent in clothes the better you look'},
+	{question:'The truth even if it hurts'},
+	{question:'Like to go out socially with more than 5 people'},
+	{question:'If you are in a rush, would you stop to help someone'},
+	{question:'How bossy can you get'}
 ];
 module.exports = questions;
